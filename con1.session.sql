2ad26a6aad068fd5ad1aebdb6294310f78915a24
Nils Sirrenberg <nils.sirrenberg@web.de>
2023-11-09 21:39:36 +0100

    added basic React structure

---
+++ con1.session.sql
@@ -0,0 +1,11 @@
+-- CREATE TABLE TESTTABLE(
+--     items varchar(50),
+--     zahl int
+-- );
+
+-- INSERT INTO TESTTABLE
+-- VALUES 
+--     ('Miguel',1000),
+--     ('Nils', 2000);
+
+SELECT * FROM testtable
