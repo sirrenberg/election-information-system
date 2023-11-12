d984827e05ffd3d11fdd340073e9aeb3be9f74f8
Nils Sirrenberg <nils.sirrenberg@web.de>
2023-11-12 13:49:44 +0100

    added a query to insert single votes from an aggregated vote table. needs to be a bit more elaborate.

---
+++ db/compute_single_votes_from_aggregate.sql
@@ -0,0 +1,28 @@
+-- Recursive CTE to insert vote tuples depending on their number in the aggregated vote table
+WITH RECURSIVE AggregierteStimmkreisergebnisse (kandidatenid, anzahlStimmen) AS (
+    (Values (1234, 2), (4567, 5))
+),
+InsertRecursive AS (
+    -- Anchor member: Initial row
+    SELECT
+        s.kandidatenid AS kandidatenid,
+        1 AS Iteration
+    FROM
+        AggregierteStimmkreisergebnisse s
+    WHERE
+        s.anzahlStimmen > 0  -- Termination condition based on the anzahlStimmen in AggregierteStimmkreisergebnisse
+
+    UNION ALL
+
+    -- Recursive member: Generate additional rows
+    SELECT
+        ir.kandidatenid,
+        ir.Iteration + 1
+    FROM
+        InsertRecursive ir
+    WHERE
+        ir.Iteration + 1 <= (SELECT anzahlStimmen FROM AggregierteStimmkreisergebnisse WHERE kandidatenid = ir.kandidatenid)  -- Termination condition based on the anzahlStimmen in AggregierteStimmkreisergebnisse
+)
+
+-- Select from DestinationTable to see the results
+SELECT kandidatenid, Iteration FROM InsertRecursive;
