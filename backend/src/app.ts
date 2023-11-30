78cef102980c10878302aedf11ffc9969c7930ce
Miguel Marcano <miguel131299@gmail.com>
2023-11-30 16:53:05 +0100

    Add seats route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
 import partyRoutes from "./routes/parties.js";
+import seatRoutes from "./routes/seats.js";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/parties", partyRoutes);
+app.use("/seats", seatRoutes);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
