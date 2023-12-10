a10ba4da1827ec85e42e78f3075f0704cd9aaa1e
ge64qij <boris.trifonov@tum.de>
2023-12-10 01:33:09 +0100

    Created script for merging the "stimmen" insert queries

--- db/db_setup/prepare_data.py
+++ db/db_setup/prepare_data.py
@@ -3,6 +3,7 @@ from DataParser.DataParser import DataParser
 
 
 import os
+import shutil
 
 #The script should
 #Clean data
@@ -18,6 +19,87 @@ def main():
     DataParser.create_insert_queries(2023)
     DataParser.create_insert_queries(2018)
     DataParser.close_candidate_query()
+
+    #Merge SQL insert queries for easier manual execution
+    merge_queries()
+
+def merge_queries():
+    erststimmen_2018 = os.path.join(".", "insert_queries", "erststimmen_2018") 
+    erststimmen_2023 = os.path.join(".", "insert_queries", "erststimmen_2023")
+
+    zweitstimmen_2018 = os.path.join(".", "insert_queries", "zweitstimmen_2018")
+    zweitstimmen_2023 = os.path.join(".", "insert_queries", "zweitstimmen_2023")
+
+    erststimmen = f"INSERT INTO kandidiert_erststimmen VALUES \n"
+    zweitstimmen = f"INSERT INTO kandidiert_zweitstimmen VALUES \n"
+
+    for file_name in os.listdir(erststimmen_2018):
+        file_path = os.path.join(erststimmen_2018, file_name)
+
+        with open(file_path, "r") as file:
+            for index, line in enumerate(file):
+                if index==0:
+                    continue
+                erststimmen+=line
+
+        #Remove the ;
+        erststimmen = erststimmen[:-1] + ",\n"
+
+    for file_name in os.listdir(erststimmen_2023):
+        file_path = os.path.join(erststimmen_2023, file_name)
+
+        with open(file_path, "r") as file:
+            for index, line in enumerate(file):
+                if index==0:
+                    continue
+                erststimmen+=line
+
+        #Remove the ;
+        erststimmen = erststimmen[:-1] + ",\n"
+
+    erststimmen = erststimmen[:-2] + ";\n"
+
+    erststimmen_file_path = os.path.join(".", "insert_queries", "erststimmen.sql")
+    with open(erststimmen_file_path, 'wb') as file:
+        file.write(erststimmen.encode('utf-8', 'ignore'))
+
+
+    for file_name in os.listdir(zweitstimmen_2018):
+        file_path = os.path.join(zweitstimmen_2018, file_name)
+
+        with open(file_path, "r") as file:
+            for index, line in enumerate(file):
+                if index==0:
+                    continue
+                zweitstimmen+=line
+
+        #Remove the ;
+        zweitstimmen = zweitstimmen[:-1] + ",\n"
+
+    for file_name in os.listdir(zweitstimmen_2023):
+        file_path = os.path.join(zweitstimmen_2023, file_name)
+
+        with open(file_path, "r") as file:
+            for index, line in enumerate(file):
+                if index==0:
+                    continue
+                zweitstimmen+=line
+
+        #Remove the ;
+        zweitstimmen = zweitstimmen[:-1] + ",\n"
+
+    zweitstimmen = zweitstimmen[:-2] + ";\n"
+
+    zweitstimmen_file_path = os.path.join(".", "insert_queries", "zweitstimmen.sql")
+    with open(zweitstimmen_file_path, 'wb') as file:
+        file.write(zweitstimmen.encode('utf-8', 'ignore'))
+
+    #Now all stimmen subfolders can be deleted
+    shutil.rmtree(erststimmen_2018)
+    shutil.rmtree(erststimmen_2023)
+    
+    shutil.rmtree(zweitstimmen_2018)
+    shutil.rmtree(zweitstimmen_2023)
 
 if __name__ == "__main__":
     main()
