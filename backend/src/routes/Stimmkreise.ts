2c8032a8b49adba8adbddf0cc46eab6c6cb42b2a
ge64qij <boris.trifonov@tum.de>
2023-12-11 09:12:55 +0100

    Create Q3.1, Q3.3 and Q3.4

--- backend/src/routes/Stimmkreise.ts
+++ backend/src/routes/Stimmkreise.ts
@@ -1,6 +1,7 @@
 // src/routes/users.ts
-import express from "express";
+import express, { response } from "express";
 import pool from "../db.js";
+import containsOnlyWhitelistChars from "../whitelisting.js";
 
 const router = express.Router();
 
@@ -22,5 +23,51 @@ router.get("/", async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+router.get("/:stimmkreisid", async (req, res) => {
+  if (!containsOnlyWhitelistChars(req.params.stimmkreisid)) {
+    res.status(400).send("Bad Request");
+    return;
+  }
+
+  const resultObject = {
+    firstQuery: {},
+    thirdQuery1: {},
+    thirdQuery2: {},
+    fourthQuery: {}
+  };
+
+  const { rows: firstQueryRes } = await pool.query(
+  `SELECT w.*
+   FROM wahlbeteiligungProStimmkreis w
+    WHERE w.stimmkreisid = $1`,[req.params.stimmkreisid])
+
+  resultObject.firstQuery = firstQueryRes
+
+  //Gesamtstimmen absolut
+  const { rows: thirdQueryRes1 } = await pool.query(
+    `SELECT g.*
+    FROM gesamtStimmenProParteiProStimmkreis g
+     WHERE g.stimmkreisid = $1` ,[req.params.stimmkreisid])
+
+  resultObject.thirdQuery1 = thirdQueryRes1  
+
+  //Gesamtstimmen prozentual
+  const { rows: thirdQueryRes2 } = await pool.query(
+    `SELECT g.*
+     FROM pgesamtStimmenProParteiProStimmkreis g
+      WHERE g.stimmkreisid = $1`, [req.params.stimmkreisid])
+
+  resultObject.thirdQuery2 = thirdQueryRes2
+
+  const { rows: fourthQueryRes } = await pool.query(
+    `SELECT g.*
+    FROM stimmenUnterschiedProParteiProStimmkreis g
+     WHERE g.stimmkreisid = $1`, [req.params.stimmkreisid])
+
+  resultObject.fourthQuery = fourthQueryRes
+  res.json(resultObject)
+
+})
 
 export default router;
